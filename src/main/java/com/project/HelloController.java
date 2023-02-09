d0cc5ea4f527a653173c392c77df395791652279
Soni Nisha <soni.nishaa24@gmail.com>
2023-02-09 23:40:16 +0530

    first commit

---
+++ src/main/java/com/project/HelloController.java
@@ -0,0 +1,21 @@
+package com.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+	@RequestMapping("/hello")
+	public String redirect() {
+		
+		return "view";
+		
+	}
+	@RequestMapping("/helloagain")
+	public String display() {
+		
+		return "final";
+	}
+}
